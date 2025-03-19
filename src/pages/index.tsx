86942495a709dc530a0132b7d0b887bc4f0c3a90
Fernando <fernandodevpascoal@gmail.com>
2025-03-19 16:25:22 -0400

    remove new sdk

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 
 import { BaseButton } from '@w3block/w3block-ui-sdk';
-import { Hello } from 'new-sdk-w3block';
 import React from 'react';
 import '@w3block/w3block-ui-sdk/dist/style.css';
 
@@ -10,7 +9,6 @@ const HomePage = () => {
   return (
     <div style={{ padding: "20px" }}>
       <h1>Bem-vindo ao Consumidor do SDK</h1>
-      <Hello />
       <BaseButton size='large' variant='outlined' type='button' fullWidth>
         Teste new SDK 18.20.7
       </BaseButton> 
