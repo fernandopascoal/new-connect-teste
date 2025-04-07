f535cfa9cd02c8db25a3265a05410e0d23f8abca
Fernando <fernandodevpascoal@gmail.com>
2025-04-07 14:01:29 -0400

    teste getPage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -114,6 +114,7 @@ export const getServerSideProps: GetServerSideProps = async ({
   const returns: any = {};
   try {
     const pageCall = await GetPage(host, (token as any)?.accessToken);
+    console.log(pageCall, 'pageCall')
     returns.page = pageCall;
   } catch (error) {
     console.log(error);
@@ -127,6 +128,8 @@ export const getServerSideProps: GetServerSideProps = async ({
   const url = resolvedUrl
     ? "https://" + req.headers.host + resolvedUrl
     : "https://" + req.headers.host;
+
+    console.log(returns, 'returns')
 
   return {
     props: {
